1b06cae251e53bbc377dfe1b4ac195c692b9f916
fasnimo <fasnimo@gmail.com>
2020-03-05 22:23:24 -0600

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dock_list = [["Shanghai", "Cell Phones"],["Singapore", "Cars"],["Shenzhen", "Medication"]]
+
+dock_list.each do |title, location|
+    Dock.create(title: title, location: location)
+end 
+
+# shanghai = Dock.create(title: "Shanghai", location: "Yangzte Delta")
+# shanghai = Dock.create(title: "Singapore", location: "Singapore Strait")
+# shanghai = Dock.create(title: "Shenzhen", location: "Pearl River Delta")
+
