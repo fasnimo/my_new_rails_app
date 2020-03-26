a908b97eb3362794d0936022e82032afc884aaa1
fasnimo <fasnimo@gmail.com>
2020-03-26 10:08:25 -0500

    Validation change

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,6 +2,7 @@ class Mission < ApplicationRecord
     belongs_to :port, optional: true
     belongs_to :company, optional: true
     validates :ship, presence: {message: "Need shipment info."}
+    validates :ship, length: {maximum: 25, too_long: "Characters maximum is 25"}
     validates :ship, length: {minimum: 2 }    
     scope :complete, -> {where(complete: true)}
  
